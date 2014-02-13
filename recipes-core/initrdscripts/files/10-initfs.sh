edcb29d6dae5675ae23ce09ce721c7adc54174fa
Elvis Dowson <elvis.dowson@gmail.com>
2014-02-14 01:18:53 +0400

    meta-xilinx-community: Delete local recipes to layer on top of meta-xilinx.
    This commit:
    - deletes all core, kernel and bsp recipes to prepare for layering on top
      of the offical meta-xilinx layer.
    
    Signed-off-by: Elvis Dowson <elvis.dowson@gmail.com>

--- recipes-core/initrdscripts/files/10-initfs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-modprobe -q vfat >/dev/null 2>&1
-modprobe -q ext2 >/dev/null 2>&1
-modprobe -q ext3 >/dev/null 2>&1
-modprobe -q jffs2 >/dev/null 2>&1
--- recipes-core/util-linux/util-linux_2.22.2.bbappend
+++
@@ -1,4 +0,0 @@
-FILESEXTRAPATHS := "${THISDIR}/files"
-
-# Apply patches
-SRC_URI += "file://0001-fdisks-fdiskbsdlabel.h-Add-support-for-microblaze-an.patch"
